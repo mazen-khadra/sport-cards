5342a44ca64a6b08c9558edc884d385a176c45ba
Mazen ALkhadra <mazen.alkhadra@outlook.com>
2023-07-25 17:35:48 +0400

    Match Opponent User

--- app/Models/Matches.php
+++ app/Models/Matches.php
@@ -10,11 +10,15 @@ class Matches extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'result', 'score'];
+    protected $fillable = ['user_id', 'result', 'score', 'opponent_user_id'];
 
-    protected $appends = ['user'];
+    protected $appends = ['user', 'opponent_user'];
 
     public function getUserAttribute() {
         return UserModel::where('id', $this->attributes['user_id'])->first();
+    }
+
+    public function getOpponentUserAttribute() {
+        return UserModel::where('id', $this->attributes['opponent_user_id'])->first();
     }
 }
--- database/migrations/2023_07_25_124203_create_matches_table.php
+++ database/migrations/2023_07_25_124203_create_matches_table.php
@@ -14,11 +14,15 @@ return new class extends Migration
         Schema::create('matches', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('opponent_user_id')->nullable()->default(null);
             $table->enum('result', ['LOSE', 'WIN', 'DRAW']);
             $table->double('score')->default(0);
+
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('opponent_user_id')->references('id')->on('users')
                 ->onDelete('cascade')->onUpdate('cascade');
         });
     }
